81cb7819ddb5fdcc19beaf6f7575e50a6e6c2693
Kage <Kage@DESKTOP-Q7I0O12.3520.dhcp.asu.edu>
2024-09-24 15:36:37 -0700

    Added Hello World project

---
+++ ASUHelloWorldJavaFX/src/asuHelloWorldJavaFX/ASUHelloWorldJavaFX.java
@@ -0,0 +1,33 @@
+package asuHelloWorldJavaFX;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class ASUHelloWorldJavaFX extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public void start(Stage primaryStage) {
+    	System.out.println("ASU Hello World!");
+    	System.out.println("It started!");
+        primaryStage.setTitle("Tyler Taing Carter Hello World Spring 2024");
+        Button btn = new Button();
+        btn.setText("Display: 'Tyler says: Hello World!'");
+        btn.setOnAction(new EventHandler<>() {
+            public void handle(ActionEvent event) {
+                System.out.println("Tyler Says: Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
